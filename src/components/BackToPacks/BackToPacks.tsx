6ea4fc6d3607f55ac3737ac08684d0e5a9003a0c
Paferov Nikolai <paferov.nikolai@gmail.com>
2023-01-08 17:13:07 +0300

    refactoring of BackToPacks (conditional rendering)

--- src/components/BackToPacks/BackToPacks.tsx
+++ src/components/BackToPacks/BackToPacks.tsx
@@ -2,16 +2,24 @@ import React from "react";
 import { PATHS } from "../../app/App";
 import { useNavigate } from "react-router-dom";
 import styles from "./BackToPacks.module.css";
+import { useAppSelector } from "../../bll/store";
+import { selectRequestProcessingStatus } from "../../utils/selectors";
 
 export const BackToPacks = () => {
+  const isRequestProcessing = useAppSelector(selectRequestProcessingStatus);
   const navigate = useNavigate();
 
   const onMoveToPacksList = () => {
     navigate(PATHS.PACKS);
   };
+
   return (
     <div className={styles.navToPacks}>
-      <span onClick={onMoveToPacksList}>🡨 Back to Packs List</span>
+      {!isRequestProcessing ? (
+        <span onClick={onMoveToPacksList}>🡨 Back to Packs List</span>
+      ) : (
+        <span>Please wait for the page refresh</span>
+      )}
     </div>
   );
 };

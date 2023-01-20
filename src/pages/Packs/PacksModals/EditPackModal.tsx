1b48a17a61441afd5b95a2e665287be91dd6b56f
Paferov Nikolai <paferov.nikolai@gmail.com>
2023-01-20 13:25:10 +0300

    add ability to change pack cover in EditPackModal

--- src/pages/Packs/PacksModals/EditPackModal.tsx
+++ src/pages/Packs/PacksModals/EditPackModal.tsx
@@ -4,6 +4,8 @@ import styles from "./PacksModals.module.css";
 import { useAppDispatch } from "../../../bll/store";
 import { updatePackTC } from "../../../bll/packs-reducer";
 import { PackType } from "../../../api/packs-api";
+import { InputTypeFile } from "../../../components/InputTypeFile/InputTypeFile";
+import Button from "@mui/material/Button/Button";
 
 type PropsType = {
   pack: null | PackType;
@@ -16,6 +18,7 @@ export const EditPackModal: FC<PropsType> = ({ pack, isOpenModal, setIsOpenModal
 
   const [packName, setPackName] = useState(pack ? pack.name : "");
   const [privateStatus, setPrivateStatus] = useState(pack ? pack.private : false);
+  const [packCover, setPackCover] = useState(pack ? pack.deckCover : "");
 
   const onChangePackName = (e: ChangeEvent<HTMLInputElement>) => {
     setPackName(e.currentTarget.value);
@@ -28,7 +31,14 @@ export const EditPackModal: FC<PropsType> = ({ pack, isOpenModal, setIsOpenModal
   const onUpdatePack = () => {
     const newPackName = packName.trim();
     if (pack) {
-      dispatch(updatePackTC({ _id: pack._id, name: newPackName, private: privateStatus }));
+      dispatch(
+        updatePackTC({
+          _id: pack._id,
+          name: newPackName,
+          deckCover: packCover,
+          private: privateStatus,
+        })
+      );
       setIsOpenModal(false);
     }
   };
@@ -37,6 +47,10 @@ export const EditPackModal: FC<PropsType> = ({ pack, isOpenModal, setIsOpenModal
     if (e.code === "Enter") {
       onUpdatePack();
     }
+  };
+
+  const onUploadPackCover = (file64: string) => {
+    setPackCover(file64);
   };
 
   return (
@@ -58,6 +72,14 @@ export const EditPackModal: FC<PropsType> = ({ pack, isOpenModal, setIsOpenModal
           onChange={onChangePackName}
           onKeyPress={onEnterPress}
         />
+        <InputTypeFile callBack={onUploadPackCover}>
+          <Button style={{ width: "100%" }} variant="contained" component="span">
+            Update pack cover
+          </Button>
+        </InputTypeFile>
+        <div className={styles.packCover}>
+          {packCover && <img style={{ maxWidth: "200px" }} alt="cover" src={packCover} />}
+        </div>
         <div>
           <input type="checkbox" checked={privateStatus} onChange={onChangePackPrivateStatus} />
           <span>Private pack</span>

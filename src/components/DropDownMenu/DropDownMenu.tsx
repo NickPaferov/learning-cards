26e763ca331003fc9849c3561e3662da061de1e0
Paferov Nikolai <paferov.nikolai@gmail.com>
2023-01-31 20:23:20 +0300

    refactoring of DropDownMenu component (add disabling)

--- src/components/DropDownMenu/DropDownMenu.tsx
+++ src/components/DropDownMenu/DropDownMenu.tsx
@@ -4,11 +4,14 @@ import MenuItem from "@mui/material/MenuItem";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { logoutTC } from "../../bll/auth-reducer";
 import { useNavigate } from "react-router-dom";
-import { useAppDispatch } from "../../bll/store";
+import { useAppDispatch, useAppSelector } from "../../bll/store";
 import IconButton from "@mui/material/IconButton/IconButton";
 import { PATHS } from "../../enums/paths";
+import { selectRequestProcessingStatus } from "../../utils/selectors";
 
 export const DropDownMenu = () => {
+  const isRequestProcessing = useAppSelector(selectRequestProcessingStatus);
+
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -39,6 +42,7 @@ export const DropDownMenu = () => {
         aria-expanded={open ? "true" : undefined}
         onClick={handleClick}
         color={"inherit"}
+        disabled={isRequestProcessing}
       >
         <MoreVertIcon />
       </IconButton>
